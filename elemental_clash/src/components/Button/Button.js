6adbd123f7bf7c4960e8571ff1b7cda63a61c698
Michael Zayed <zayedmichael@gmail.com>
2025-04-10 18:27:27 -0500

    Created Variability in the game name generator and changed Create Game Button functionality by displaying the game name in the text box for the user. Each game name is checked if it already exists. When code is displayed, it is also added to Supabase

--- elemental_clash/src/components/Button/Button.js
+++ elemental_clash/src/components/Button/Button.js
@@ -1,14 +1,36 @@
 import styles from './Button.module.css';
+import {useState} from "react"
+import { generateFiveDigitCode } from '../GameStateComponent';
 
-function Button({ text = 'Test', onClick }) {
+function Button({ text = 'Test'}) {
+    const [currentID, setNewID] = useState(text);
+    const whenClicked = () => {
+        console.log("HHHHHHHHHHHH");
+        setNewID(generateFiveDigitCode());
+    };
+
     return (
         <button 
             className={styles.button}
-            onClick={onClick}
+            onClick={whenClicked}
         >
-            {text}
+            {currentID}
         </button>
     );
 }
 
 export default Button;
+
+// function Button({ text = 'Test', onClick }) {
+    
+//     return (
+//         <button 
+//             className={styles.button}
+//             onClick={onClick}
+//         >
+//             {text}
+//         </button>
+//     );
+// }
+
+// export default Button;
