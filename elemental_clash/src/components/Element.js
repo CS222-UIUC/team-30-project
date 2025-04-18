6c1ac1a6368a119ad30e9e7a11efb9a397bb806d
Connor Koefelda <bladefin99@gmail.com>
2025-04-18 14:35:35 -0500

    added some comments

--- elemental_clash/src/components/Element.js
+++ elemental_clash/src/components/Element.js
@@ -6,6 +6,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
     const [startPos, setStartPos] = useState({x: 0, y: 0});
     const [elementStartPos, setElementStartPos] = useState({x: position?.x || 0, y: position?.y || 0});
 
+    /**
+     * When the mouse is moved, if an element is currently being dragged, update its position based on the mouse movement
+     */
     const handleMouseMove = useCallback((event) => {
         if (dragging) {
             const dx = event.clientX - startPos.x;
@@ -14,6 +17,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
         }
     }, [dragging, startPos, elementStartPos]);
 
+    /**
+     * When the mouse is pressed, if the mouse is over the element then save its starting position and set dragging to true
+     */
     const handlePress = useCallback((event) => {
         event.preventDefault();
         const rect = event.currentTarget.getBoundingClientRect();
@@ -26,6 +32,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
         console.log(`it is ${dragging} that am dragging`)
     }, []);
 
+    /**
+     * When mouse is released, set dragging to false and update the element position accordingly (if its an inventory element, move it back, then call onDragStop either way)
+     */
     const handleRelease = useCallback(() => {
         if (dragging) {
             setDragging(false);
@@ -41,6 +50,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
         console.log(`it is ${dragging} that am dragging`)
     }, [dragging, id, onDragStop, currentPosition, inventory, elementStartPos, startPos]);
 
+    /**
+     * Enables controlling elements based on mouse clicks
+     */
     useEffect(() => {
         if (dragging) {
             window.addEventListener('mousemove', handleMouseMove);
@@ -54,9 +66,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
     }, [dragging, handleMouseMove, handleRelease]);
 
     const style = {
-        position: (!inventory || dragging) ? 'absolute' : 'relative',
-        left: (!inventory || dragging) ? `${currentPosition.x}px` : 'auto',
-        top: (!inventory || dragging) ? `${currentPosition.y}px` : 'auto',
+        position: (!inventory || dragging) ? 'absolute' : 'relative', //if its not in the inventory or its actively being moved, make the position absolute.  Otherwise, fit it in the grid
+        left: (!inventory || dragging) ? `${currentPosition.x}px` : 'auto', //see position comment
+        top: (!inventory || dragging) ? `${currentPosition.y}px` : 'auto', //see position comment
         display: 'inline-block',
         paddingTop: '1px',
         paddingBottom: '1px',
@@ -69,9 +81,9 @@ const Element = forwardRef(( { text, id, position, onDragStop, inventory }, ref 
         color: '#333',
         fontSize: '16px',
         textAlign: 'center',
-        cursor: dragging ? 'grabbing' : 'grab',
-        transition: dragging ? 'none' : 'transform .3s ease',
-        width: 'fit-content'
+        cursor: dragging ? 'grabbing' : 'grab', //styling
+        transition: dragging ? 'none' : 'transform .3s ease', //avoid goofy ah teleporting
+        width: 'fit-content' //keeps the inventory elements from filling their columns
     };
 
     return (
